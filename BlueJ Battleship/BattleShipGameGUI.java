0fb79952c0e707b7f8ce7dddc3b4828144d4d433
Daniel Trupp <1851968@00-63159.local>
2017-05-10 11:15:02 -0700

    My edits

--- BlueJ Battleship/BattleShipGameGUI.java
+++ BlueJ Battleship/BattleShipGameGUI.java
@@ -18,19 +18,27 @@ public class BattleShipGameGUI extends JFrame implements ActionListener
     /* Width of the game frame. */
     private static final int DEFAULT_WIDTH = 1440;
     
+    /** Width of background. */
+    private static final int BACK_WIDTH = 500;
+	/** Height of background. */
+	private static final int BACK_HEIGHT = 500;
+    
     /** The main panel containing the game components. */
-	private JPanel panel;
-	
-	/** The boards. */
-	private PlayerBoard friend;
-	private OpponentBoard enemy;
-	
-	/** Board sizes*/
-	private int boardWidth;
-	private int boardHeight;
-	
+    private JPanel panel;
+    
+    /** The boards. */
+    private PlayerBoard friend;
+    private OpponentBoard enemy;
+    
+    /** Board sizes*/
+    private int boardWidth;
+    private int boardHeight;
+    
+    /** The displays. */
+    private JLabel[] display;
+    
     /** Button array of accessable Squares. */
-	private JButton[][] validSpaces = new JButton[10][10];
+    private JButton[][] validSpaces = new JButton[10][10];
     
     public BattleShipGameGUI(PlayerBoard p1, OpponentBoard p2)
     {
@@ -44,79 +52,149 @@ public class BattleShipGameGUI extends JFrame implements ActionListener
                 validSpaces[x][y] = new JButton();
                 
         initDisplay();
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        repaint();
     }
     
     /**
-	 * Run the game.
-	 */
-	public void displayGame() {
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				setVisible(true);
-			}
-		});
-	}
-	
-	/**
-	 * Initialize the display.
-	 */
-	private void initDisplay()	{
-		panel = new JPanel() {
-			public void paintComponent(Graphics g) {
-				super.paintComponent(g);
-			}
-		};
+     * Run the game.
+     */
+    public void displayGame() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                setVisible(true);
+            }
+        });
+    }
+    
+    /**
+     * Initialize the display.
+     */
+    private void initDisplay()  {
+        panel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+            }
+        };
 
-		// If board object's class name follows the standard format
-		// of ...Board or ...board, use the prefix for the JFrame title
-		String className = friend.getClass().getSimpleName();
-		int classNameLen = className.length();
-		int boardLen = "Board".length();
-		String boardStr = className.substring(classNameLen - boardLen);
-		if (boardStr.equals("Board") || boardStr.equals("board")) {
-			int titleLength = classNameLen - boardLen;
-			setTitle(className.substring(0, titleLength));
-		}
+        // Calculate number of rows of cards (5 cards per row)
+        // and adjust JFrame height if necessary
 
-		// Calculate number of rows of cards (5 cards per row)
-		// and adjust JFrame height if necessary
+        this.setSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+        panel.setLayout(null);
+        panel.setPreferredSize(new Dimension(DEFAULT_WIDTH - 20, DEFAULT_HEIGHT - 20));
 
-		this.setSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
-		panel.setLayout(null);
-		panel.setPreferredSize(new Dimension(DEFAULT_WIDTH - 20, DEFAULT_HEIGHT - 20));
-
-		//Do the buttons
-		for (int x = 0; x < boardHeight; x++)
-		    for (int y = 0; y < boardWidth; y++)
-    		{
-    		    JButton butt = validSpaces[x][y];
-        		butt.setText("Test");
-        		panel.add(butt);
-        		butt.setBounds(0 + 30 * x, 0 + 30 * y, 30, 30);
-        		butt.addActionListener(this);
+        //Do the buttons
+        for (int x = 0; x < boardHeight; x++)
+            for (int y = 0; y < boardWidth; y++)
+            {
+                JButton butt = validSpaces[x][y];
+                butt.setText((char)(65+y) + "" + (int)(x+1));
+                butt.setBackground(new Color(255,255,255,255));
+                panel.add(butt);
+                butt.setBounds(28 + 50 * x, 28 + 50 * y, 55, 55);
+                butt.addActionListener(this);
             }
 
-		pack();
-		getContentPane().add(panel);
-		panel.setVisible(true);
-	}
-	
-	/**
-	 * Deal with the user clicking on something other than a button or a card.
+        display = new JLabel[1];
+        for (int k = 0; k < 1; k++) 
+        {
+            display[k] = new JLabel();
+            panel.add(display[k]);
+			display[k].setBounds(30, 30, BACK_WIDTH, BACK_HEIGHT);
+			display[k].addMouseListener(new MyMouseListener());
+        }
+        pack();
+        getContentPane().add(panel);
+        panel.setVisible(true);
+    }
+    
+    /**
+     * Draw the display (cards and messages).
+     */
+    public void repaint() {
+        for (int k = 0; k < 1; k++) 
+        {
+            URL imageURL = getClass().getResource("/Images/ocean.GIF");
+            if (imageURL != null) {
+                ImageIcon icon = new ImageIcon(imageURL);
+                display[k].setIcon(icon);
+                display[k].setVisible(true);
+            } else {
+                throw new RuntimeException("Image not found");
+            }
+        }
+        pack();
+        panel.repaint();
+    }
+    
+    /**
+     * Deal with the user clicking on something other than a button or a card.
+     */
+    private void signalError() {
+        Toolkit t = panel.getToolkit();
+        t.beep();
+    }
+    
+    /**
+     * Respond to a button click (on either the "Replace" button
+     * or the "Restart" button).
+     * @param e the button click action event
+     */
+    public void actionPerformed(ActionEvent e) 
+    {
+        
+    }
+    
+    /**
+	 * Receives and handles mouse clicks.  Other mouse events are ignored.
 	 */
-	private void signalError() {
-		Toolkit t = panel.getToolkit();
-		t.beep();
-	}
-	
-	/**
-	 * Respond to a button click (on either the "Replace" button
-	 * or the "Restart" button).
-	 * @param e the button click action event
-	 */
-	public void actionPerformed(ActionEvent e) 
+	private class MyMouseListener implements MouseListener 
 	{
-		
+
+		/**
+		 * Handle a mouse click on a card by toggling its "selected" property.
+		 * Each card is represented as a label.
+		 * @param e the mouse event.
+		 */
+		public void mouseClicked(MouseEvent e) {
+			for (int k = 0; k < 5; k++) 
+			{
+				if (e.getSource().equals(display[k])) 
+				{
+					repaint();
+					return;
+				}
+			}
+			signalError();
+		}
+
+		/**
+		 * Ignore a mouse exited event.
+		 * @param e the mouse event.
+		 */
+		public void mouseExited(MouseEvent e) {
+		}
+
+		/**
+		 * Ignore a mouse released event.
+		 * @param e the mouse event.
+		 */
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		/**
+		 * Ignore a mouse entered event.
+		 * @param e the mouse event.
+		 */
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		/**
+		 * Ignore a mouse pressed event.
+		 * @param e the mouse event.
+		 */
+		public void mousePressed(MouseEvent e) {
+		}
 	}
 }

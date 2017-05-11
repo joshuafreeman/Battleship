ecaf31253a86ffbd662a639866adc717f855f34f
AntonGera <1828784@00-63158.local>
2017-05-11 11:02:42 -0700

    Main fixed

--- BlueJ Battleship/Main.java
+++ BlueJ Battleship/Main.java
@@ -1,38 +1,160 @@
-public class Main {
-
+import chn.util.*;
+public class Main 
+{
     public static void main(String[] args)
     {
+        ConsoleIO con = new ConsoleIO();
         PlayerBoard bor = new PlayerBoard(10, 10);
+        int xCord,yCord,boatType,rot;
+        boolean right = true;
         bor.emptyBoard();
+        System.out.print(" ");
+         for(int y = 1; y <= bor.getWidth(); y++)
+        {
+            System.out.print(y + " ");
+        }
+        System.out.println();
         for(int y = 0; y < bor.getHeight(); y++)
         {
+            System.out.print(y+1);
             for (int x = 0; x < bor.getWidth(); x++)
                 System.out.print(bor.displaySpot(x, y) + " ");
             System.out.println();
         }
         System.out.println();
-        bor.setShip(1, 1, 0, "Battleship");
-        bor.setShip(4, 4, 1, "Submarine");
-        System.out.println(bor.setShip(6, 6, 0, "Carrier"));
-        System.out.println(bor.setShip(6,6,0, "Patrol Boat")); //Test to see if you can place a ship on another ship
-        System.out.println(bor.setShip(10,10,0, "Battleship")); //Test to see if you can place a ship off array
-        System.out.println(bor.rotateShip(4,4));
-        System.out.println(bor.rotateShip(6,6));
-        System.out.println(bor.receiveAttack(1,1));
-        System.out.println(bor.receiveAttack(2,1));
-        System.out.println(bor.receiveAttack(3,1));
-        System.out.println(bor.receiveAttack(4,1));
-        System.out.println(bor.receiveAttack(5,1));
-        System.out.println(bor.receiveAttack(6,1));
-        System.out.println(bor.receiveAttack(2,2));
-        for(int y = 0; y < bor.getHeight(); y++)
+        for(int x = 0; x < 5; x++)
         {
-            for (int x = 0; x < bor.getWidth(); x++)
-                System.out.print(bor.displaySpot(x, y) + " ");
-            System.out.println();
+            System.out.println("Please type a X cord.");
+            xCord = con.readInt();
+            while(right)
+            {
+                if(xCord < 1 || xCord > 10)
+                {
+                   System.out.println("Please type a X cord between 1 and 10.");
+                   xCord = con.readInt(); 
+                }
+                else
+                {
+                    right = false;
+                }    
+            }  
+            right = true;
+            System.out.println("Please type a Y cord.");
+            yCord = con.readInt();
+             while(right)
+            {
+                if(yCord < 1 || yCord > 10)
+                {
+                   System.out.println("Please type a Y cord between 1 and 10.");
+                   yCord = con.readInt(); 
+                }
+                else
+                {
+                    right = false;
+                }    
+            }  
+            right = true;
+            System.out.println("Please type 0 for Horizontal and 1 for Vertical");
+            rot = con.readInt();
+             while(right)
+            {
+                if(rot < 0 || rot > 1)
+                {
+                   System.out.println("Please learn to type.");
+                   rot = con.readInt(); 
+                }
+                else
+                {
+                    right = false;
+                }    
+            }  
+            right = true;
+            System.out.println("BattleShip:1,Submarine:2,Carrier:3,PatrolBoat:4,Destroyer:5");
+            boatType = con.readInt();
+             while(right)
+            {
+                if(boatType < 1 || boatType > 5)
+                {
+                   System.out.println("Please type between 1 and 5.");
+                   boatType = con.readInt(); 
+                }
+                else
+                {
+                    right = false;
+                }    
+            }  
+            right = true;
+            switch(boatType)
+            {
+                case(1):
+                    if(bor.setShip(xCord,yCord,rot,"Battleship"))
+                    
+                    break;
+                case(2):
+                    if(bor.setShip(xCord,yCord,rot,"Submarine"))
+                    break;                
+                case(3):
+                    if(bor.setShip(xCord,yCord,rot,"Carrier"))
+                    break;                
+                case(4):
+                    if(bor.setShip(xCord,yCord,rot,"Patrol Boat"))
+                    break;                
+                case(5):
+                    if(bor.setShip(xCord,yCord,rot,"Destroyer"))
+                    break;              
+            }    
+            for(int y = 0; y < bor.getHeight(); y++)
+            {
+                for (int x2 = 0; x2 < bor.getWidth(); x2++)
+                    System.out.print(bor.displaySpot(x2, y) + " ");
+                System.out.println();
+            }
         }
-        // write your code here
-
-        //test
+        while(!bor.isEmpty())
+        {
+            System.out.println("Please type a X cord to Shoot.");
+            xCord = con.readInt();
+            while(right)
+            {
+                if(xCord < 1 || xCord > 10)
+                {
+                   System.out.println("Please type a X cord between 1 and 10.");
+                   xCord = con.readInt(); 
+                }
+                else
+                {
+                    right = false;
+                }    
+            }  
+            right = true;
+            System.out.println("Please type a Y cord to Shoot.");
+            yCord = con.readInt();
+             while(right)
+            {
+                if(yCord < 1 || yCord > 10)
+                {
+                   System.out.println("Please type a Y cord between 1 and 10.");
+                   yCord = con.readInt(); 
+                }
+                else
+                {
+                    right = false;
+                }    
+            }  
+            right = true;
+            if(bor.receiveAttack(xCord,yCord))
+            {
+                System.out.println("U SONK MOI BITLESHOP");
+            }
+            
+            for(int y = 0; y < bor.getHeight(); y++)
+            {
+                for (int x2 = 0; x2 < bor.getWidth(); x2++)
+                System.out.print(bor.displaySpot(x2, y) + " ");
+                System.out.println();
+            }
+            
+        }    
+        System.out.println("hoi tots pritu gud, u won");
     }
 }

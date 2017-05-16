25b428467413758cff3777d236348e5eb22eacaf
joshuafreeman <1826972@00-63158.local>
2017-05-16 12:11:50 -0700

    Modified sunk and receiveAttack method.

--- Client 1/PlayerBoard.java
+++ Client 1/PlayerBoard.java
@@ -149,19 +149,17 @@ public class PlayerBoard extends Board{
     public boolean receiveAttack(int x, int y)
     {
         x--;y--;
-        boolean sunken = false;
+        boolean hit = false;
         Spot temp = myBoard[y][x];
         if(myBoard[y][x].getType().equals("hull") || myBoard[y][x].getType().equals("head") || myBoard[y][x].getType().equals("hit"))
         {
             myBoard[y][x] = new Hit();
+            hit = true;
         }
         else
             myBoard[y][x] = new Miss();
 
-        if(temp.getType().equals("hull") || temp.getType().equals("head"))
-            sunken = sunk(temp);
-
-        return sunken;
+        return hit;
     }
     
     public boolean sendAttack(int x, int y, OpponentBoard board)
@@ -173,13 +171,12 @@ public class PlayerBoard extends Board{
         return hit;
     }
 
-    private boolean sunk(Spot place)
+    public boolean sunk(int xCord, int yCord)
     {
-        int xCord;
-        int yCord;
         boolean sunken = true;
         int rotation;
         int size;
+        Spot place = myBoard[yCord][xCord];
         Ship ship = (Ship)place;
         if(place.getType().equals("head"))
         {

b97e66e5fe6e0f41a8c0f8d3f768b716ec4035a7
joshuafreeman <1826972@00-63158.local>
2017-05-17 11:38:45 -0700

    Fixed sunk from crashing server lol

--- Client 1/GreetingServer.java
+++ Client 1/GreetingServer.java
@@ -76,8 +76,10 @@ public class GreetingServer extends Thread {
                   System.out.println("Player 1 hit Player 2's boat");
                else
                   System.out.println("Player 1 missed Player 2's boats");
-               outP1.writeBoolean(player2Board.sunk(temp1));
-
+                  
+               if((temp1.myType.equals("hull") || temp1.myType.equals("head") || temp1.myType.equals("ship")) && player2Board.sunk(temp1))
+                   outP1.writeBoolean(player2Board.sunk(temp1));
+               
                outP2.writeBoolean(true); //P2 can attack
                xCordP2 = inP2.readInt();
                yCordP2 = inP2.readInt();
@@ -88,8 +90,9 @@ public class GreetingServer extends Thread {
                   System.out.println("Player 2 hit Player 1's boat");
                else
                   System.out.println("Player 2 missed Player 1's boats");
+               if((temp2.myType.equals("hull") || temp2.myType.equals("head") || temp2.myType.equals("ship")) && player1Board.sunk(temp2))
+                   outP2.writeBoolean(player1Board.sunk(temp2));
                
-               outP2.writeBoolean(player1Board.sunk(temp2));
 
                player1Opp = new OpponentBoard(player2Board);
                player2Opp = new OpponentBoard(player1Board);

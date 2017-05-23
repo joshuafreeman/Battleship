542a5425665d7f0cb6c139e374aaa155d1e58312
Daniel Trupp <1851968@00-63159.local>
2017-05-23 12:05:16 -0700

    The GUI

--- Client 1/BattleShipGuiRunner.java
+++ Client 1/BattleShipGuiRunner.java
@@ -1,3 +1,8 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.net.URL;
+import java.util.*;
 /**
  * This is a class that plays the GUI version of the BattleShip game.
  * See accompanying documents for a description of how BattleShip is played.
@@ -10,11 +15,19 @@ public class BattleShipGuiRunner {
 	 */
 	public static void main(String[] args) 
 	{
-		PlayerBoard board = new PlayerBoard(10,10);
-		//Get PlayerBoard from server
-		PlayerBoard serverboard = new PlayerBoard();
+		JPanel uniPanel = new JPanel(){
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+            }
+        };
+		
+        StartGUI anton = new StartGUI(uniPanel);
+		anton.displayGame();
+        
+	    PlayerBoard serverboard = new PlayerBoard();
+        PlayerBoard board = new PlayerBoard(10,10);
 		OpponentBoard board2 = new OpponentBoard(serverboard);
-		BattleShipGameGUI gui = new BattleShipGameGUI(board, board2);
+		BattleShipGameGUI gui = new BattleShipGameGUI(board, board2, uniPanel);
 		gui.displayGame();
 	}
 }
